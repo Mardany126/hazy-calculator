b86634b6560b19f71128ecc060e4198f6349b764
Max Mardany <Max@Maxs-MacBook-Air.local>
2021-05-05 16:29:57 -0400

    checking if input holds a value that can be calculated and if not return false

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function calculate() {}
+function isNumericValue(item) {
+  return !isNaN(item) && item !== ''
+}
 
-module.exports
+function isCorrectItem(item) {
+  const operators = ['+', '-', '*', '/']
+
+  return isNumericValue(item) || operators.includes(item)
+}
+
+function calculate(input) {
+  let calculationSteps = []
+
+  input.forEach((item) => {
+    if (isCorrectItem(item)) {
+      calculationSteps.push(item)
+    }
+  })
+}
+
+module.exports = calculate
